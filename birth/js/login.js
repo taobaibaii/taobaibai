c7e98f3623271178dc5e7371dae3de1b2459a109
taobaibaii <46114726+taobaibaii@users.noreply.github.com>
2021-01-25 09:58:51 +0800

    Update login.js

--- birth/js/login.js
+++ birth/js/login.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="我是猪" &&  pwd=="5201314"){
+    if(userName=="zbb" &&  pwd=="980126"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
